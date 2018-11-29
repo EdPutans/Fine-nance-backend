ae64f79c0d29a52425f874f8ddab892531b1aa0e
Ed Putans <edputans@gmail.com>
2018-11-29 15:48:48 +0000

    created controllers

--- app/controllers/api/v1/spendings_controller.rb
+++ app/controllers/api/v1/spendings_controller.rb
@@ -1,3 +1,41 @@
 class Api::V1::SpendingsController < ApplicationController
+ 
     
+
+    def index
+        @spendings = Spending.all
+        render json: @spendings
+    end
+
+
+    def create
+        @spending = Spending.new(spending_params)
+        if @spending.save
+            render json: @spending
+        else
+            render json: {error: "Unable to create this Spending"}, status: 400
+        end
+    end
+
+    def show
+        @spending = Spending.find_by(id: params[:id])
+        if @spending
+            render json: @spending
+        else
+            render json: {error: "Spending was not found"}, status: 404
+        end
+    end
+
+  
+    private
+
+    def spending_params
+        params.require(:spending).permit(:id, :user_id, :user, :rent, :food, :utilities, :clothes, :travel, :other)
+    end
+
+
+
+
+
+
 end
