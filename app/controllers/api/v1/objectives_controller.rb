ca48e4ed7a2a88e570f9a2b1c87a8e43fcdb5600
Ed Putans <edputans@gmail.com>
2018-11-30 14:30:33 +0000

    updated controller

--- app/controllers/api/v1/objectives_controller.rb
+++ app/controllers/api/v1/objectives_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ObjectivesController < ApplicationController
-    
+
    def index
         @objective =Objective.all
         render json: @objective
@@ -7,13 +7,23 @@ class Api::V1::ObjectivesController < ApplicationController
 
 
     def create
-        @objective =Objective.new(objective_params)
+        @objective = Objective.new(objective_params)
         if @objective.save
             render json: @objective
         else
             render json: {error: "Unable to create this objective"}, status: 400
         end
     end
+
+    def update
+        @objective = Objective.update(objective_params)
+        if @objective
+            render json: @objective
+        else
+            render json: {error: "Unable to create this objective"}, status: 400
+        end
+    end
+
 
     def show
         @objective =Objective.find_by(id: params[:id])
